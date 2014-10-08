47c0815a35efb18d3c93dfda9e801efd7a94a08a
Oberst Tanja <taob@gmx.de>
2014-10-08 10:32:08 +0200

    Deleted ViewController and put a own class TOB_OuterSpaceTableViewController instead.
    Installed Montserrat font

---
+++ BLunch/TOB_OuterSpaceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TOB_OuterSpaceTableViewController.h
+//  BLunch
+//
+//  Created by Oberst Tanja on 07.10.14.
+//  Copyright (c) 2014 Oberst Tanja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TOB_OuterSpaceTableViewController : UITableViewController
+
+@end
